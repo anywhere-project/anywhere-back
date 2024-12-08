d3c535e46bbea675ced677775f4dc81687090d11
mikieoo <jcw2551@naver.com>
2024-12-08 22:57:02 +0900

    review like, scrap

--- src/main/java/com/project/anywhere/entity/ReviewScrapEntity.java
+++ src/main/java/com/project/anywhere/entity/ReviewScrapEntity.java
@@ -1,11 +1,15 @@
 package com.project.anywhere.entity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.project.anywhere.entity.pk.ReviewScrapPk;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +17,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "review_scraps")
 @Entity(name = "review_scraps")
 @IdClass(ReviewScrapPk.class)
@@ -22,8 +27,12 @@ public class ReviewScrapEntity {
     private String userId;
     @Id
     private Integer reviewId;
+    private String createdAt;
 
     public ReviewScrapEntity(String userId, Integer reviewId) {
+        Date now = new Date();
+        SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd hh:mm");
+        this.createdAt = simpleDateFormat.format(now);
         this.userId = userId;
         this.reviewId = reviewId;
     }
