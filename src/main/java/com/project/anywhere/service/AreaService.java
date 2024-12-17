8a41d4ecf70f1431b1c7e774e1b24baf0d6f0a9b
imwkdrh <imwkdrh7@naver.com>
2024-12-17 15:34:40 +0900

    why

--- src/main/java/com/project/anywhere/service/AreaService.java
+++ src/main/java/com/project/anywhere/service/AreaService.java
@@ -2,7 +2,6 @@ package com.project.anywhere.service;
 
 import org.springframework.http.ResponseEntity;
 
-import com.project.anywhere.dto.response.ResponseDto;
 import com.project.anywhere.dto.response.area.GetAreaResponseDto;
 
 
